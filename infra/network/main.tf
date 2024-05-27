15e6dea602eb2ce58dc405fadfd299ffd8982973
Connor Gurney <connor.gurney@gurste.com>
2024-05-27 23:40:49 +0100

    feat: scaffold infrastructure components I'll document this in more detail soon and develop each Docker container but it sets out how my infrastructure will be structured.

--- infra/network/main.tf
+++ infra/network/main.tf
@@ -32,8 +32,8 @@ resource "aws_internet_gateway" "default" {
 
 # Subnet for network-related resources
 resource "aws_subnet" "network" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = var.network_subnet_range
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.network_subnet_range
 
   tags = {
     Name = "cgmeuk-${var.environment}-network"
