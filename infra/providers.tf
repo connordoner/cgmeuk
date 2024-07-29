e8fc8e3e0229d3528c0f0da8a0adfe8cd1fa9163
Connor Gurney <me@connorgurney.me.uk>
2024-07-29 19:41:54 +0100

    Split AWS provider in Terraform across regions This is used to create my SSL and TLS certificates as Amazon CloudFront requires that they be hosted there.

--- infra/providers.tf
+++ infra/providers.tf
@@ -16,9 +16,24 @@ terraform {
   }
 }
 
-# AWS configuration
+# AWS configuration for the UK
 provider "aws" {
   region = "eu-west-2"
+  
+  default_tags {
+    tags = {
+      "connorgurney:workload"    = "cgmeuk"
+      "connorgurney:environment" = "production"
+    }
+  }
+}
+
+# AWS configuration for the USA
+# This is used to create my SSL and TLS certificates as Amazon CloudFront
+# requires that they be hosted there
+provider "aws" {
+  alias  = "us"
+  region = "us-east-1"
   
   default_tags {
     tags = {
